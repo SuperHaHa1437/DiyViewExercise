06aed3d7af11e754a8a961f06f7c96006197b88a
张张 <zhangviviyo@qq.com>
2021-03-26 17:56:59 +0800

    创建PlaceDao数据存储类

---
+++ HappyWeather/app/src/main/java/com/zhang/happyweather/logic/dao/PlaceDao.kt
@@ -0,0 +1,33 @@
+package com.zhang.happyweather.logic.dao
+
+import android.content.Context
+import androidx.core.content.edit
+import com.google.gson.Gson
+import com.zhang.happyweather.HappyWeatherApplication
+import com.zhang.happyweather.logic.model.Place
+
+/**
+ * @author : zyw
+ * @function :
+ * @date : 3/26/21
+ */
+object PlaceDao {
+
+    fun savePlace(place: Place) {
+        sharedPreferences().edit {
+            putString("place", Gson().toJson(place))
+        }
+    }
+
+    fun getSavedPlace(): Place {
+        val placeJson = sharedPreferences().getString("place", "")
+        return Gson().fromJson(placeJson, Place::class.java)
+    }
+
+    fun isPlaceSaved() = sharedPreferences().contains("place")
+
+
+    private fun sharedPreferences() =
+        HappyWeatherApplication.context.getSharedPreferences("happy_weather", Context.MODE_PRIVATE)
+
+}
